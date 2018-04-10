c58a98df0c80368d9a3328befaddbc3a2ad82660
AmatorSantsho <ash002@tut.by>
2018-04-10 12:11:22 +0200

    HW05_full

---
+++ src/test/java/ru/javawebinar/topjava/service/JdbcUserServiceTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+
+/**
+ * Created by 123 on 10.04.2018.
+ */
+@ActiveProfiles(Profiles.JDBC)
+public class JdbcUserServiceTest extends BaseUserServiceTest {
+}
